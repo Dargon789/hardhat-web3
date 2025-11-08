fe0cd12176d119f3a67dce23162d00b5783296f6
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2025-11-09 03:23:34 +0700

    Update next.config.js
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,7 +2,6 @@
 const path = require("path");
 const withLinaria = require("next-linaria");
 const withPlugins = require("next-compose-plugins");
-
 const customRedirects = require("./redirects.config");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
