5f2c3168bc7a2dc035ad4ab2906ddd7784458643
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2026-06-13 20:48:35 +0700

    Fix ESLint parserOptions project path
    Updated parserOptions.project path to use src directory.
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- packages/hardhat-ignition-core/.eslintrc.js
+++ packages/hardhat-ignition-core/.eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: [`${__dirname}/../../config/eslint/eslintrc.js`],
   parserOptions: {
-<<<<<<< Updated upstream
     project: `${__dirname}/src/tsconfig.json`,
-=======
-    project: `${__dirname}/tsconfig.json`,
->>>>>>> Stashed changes
     sourceType: "module",
   },
   ignorePatterns: [".eslintrc.js", "./dist/**/*", "./node_modules/**/*"],
