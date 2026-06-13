b255019dacbd45549d0ecd2bee1eb55fc1d3eed3
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2026-06-13 21:14:51 +0700

    Fix ESLint configuration by resolving merge conflicts
    Removed conflicting sections and updated ESLint configuration.
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- packages/hardhat-ignition-viem/.eslintrc.js
+++ packages/hardhat-ignition-viem/.eslintrc.js
@@ -1,17 +1,13 @@
-<<<<<<< Updated upstream
 const {
   slowImportsCommonIgnoredModules,
 } = require("../../config/eslint/constants");
 
-=======
->>>>>>> Stashed changes
 module.exports = {
   extends: [`${__dirname}/../../config/eslint/eslintrc.js`],
   parserOptions: {
     project: `${__dirname}/tsconfig.json`,
     sourceType: "module",
   },
-<<<<<<< Updated upstream
   overrides: [
     {
       files: ["src/index.ts"],
@@ -25,7 +21,4 @@ module.exports = {
       },
     },
   ],
-=======
-  ignorePatterns: [".eslintrc.js", "**/fixture-projects/**/*"],
->>>>>>> Stashed changes
 };
